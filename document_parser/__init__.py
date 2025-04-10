ef86b370b18771a8f3daa551520c43d5c8301683
Natesh Relhan <natesh@likeminds.community>
2025-04-10 09:01:01 +0530

    chore: update document parser version

--- document_parser/__init__.py
+++ document_parser/__init__.py
@@ -2,7 +2,5 @@
 Document Parser package for processing and combining markdown documentation.
 """
 
-from document_parser.core import DocumentParser
-
 __version__ = "1.0.0"
-__all__ = ["DocumentParser"]
+__all__ = []
